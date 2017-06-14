a210050f513159e3f139459f558568d708534436
Luis Hernandez <Luis.hernandez2017@gmail.com>
2017-06-14 04:45:10 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,61 @@
+const express 	 = require("express");//express module
+const path		 = require("path");//path module
+const bodyParser = require("body-parser");//bp module
+const cors		 = require("cors");//cors module
+const passport   = require("passport");//passport module
+const mongoose   = require("mongoose");//mongoose module
+
+
+const config = require('./config/database');
+
+//connect to database
+mongoose.connect(config.database);
+
+mongoose.connection.on('connected', () =>{
+	console.log('Connected to databse' + config.database);
+});
+
+mongoose.connection.on('error', (err) =>{
+	console.log('Database error: ' + err);
+});
+//express server
+const app = express();
+
+//port number
+const port = 3000;
+
+
+//Users Route
+const users = require('./routes/users');
+
+
+
+//CORS Middleware
+app.use(cors());
+
+
+//Set Static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Body Parser Middleware
+app.use(bodyParser.json());
+
+
+//Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport')(passport);
+
+//Users route
+app.use('/users', users);
+
+//Index route
+app.get('/', (reg, res) =>{
+	res.send("Invalid Endpoint.")
+});
+
+//Serve. listen to port 3000
+app.listen(port, () => {
+	console.log(`Server started on port ${port}`);
+});
